9f428684eb7cbdb7255e0ab808ebe30e9ac868c6
Jos Hendriks <jos.hendriks@neworange.agency>
2025-04-12 21:42:17 +0200

    Initial processor

---
+++ processor/obsidian-ai/Ollama/OllamaConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Options;
+
+namespace obsidian_ai.Ollama;
+
+public class OllamaConfig
+{   
+    [Required]
+    public string Url { get; set; } = null!;
+}
+
+
+[OptionsValidator]
+public partial class ValidateSettingsOptions : IValidateOptions<OllamaConfig>
+{
+}
